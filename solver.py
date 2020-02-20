2d9b6fd48240a1f817887f69b3b7c38447990cbb
Ospino <ospino.p@gmail.com>
2020-02-20 22:08:58 +0100

    solve one day more
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- solver.py
+++ solver.py
@@ -17,7 +17,7 @@ def solve(libraries, scores, D):
     # ORDER
     # libraries = sorted(libraries, key=lambda k: k['all_sum'], reverse=True)
     # SHUFFLE
-    random.seed(int(time.time()))
+    random.seed(int(time.time()*10**60 % 10**6))
     random.shuffle(libraries)
 
     selected_libraries = []
@@ -33,18 +33,18 @@ def solve(libraries, scores, D):
 
     score = 0
     for current_day in pbar:
+        
+        if sign_up and selected_libraries[libraries_i - 1]['sign_up_days'] + last == current_day + 1:
+            selected_libraries[libraries_i - 1]['signed_up'] = True
+            last = current_day
+            sign_up = False
 
         if not sign_up:
-            if len(libraries) > libraries_i:
+            if libraries_i < len(libraries):
                 selected_libraries.append(libraries[libraries_i])
                 libraries_i += 1
                 sign_up = True
-        else:
-            if selected_libraries[libraries_i - 1]['sign_up_days'] + last == current_day:
-                selected_libraries[libraries_i - 1]['signed_up'] = True
-                last = current_day
-                sign_up = False
-
+        
         for i in range(len(selected_libraries)):
             lib = selected_libraries[i]
             if lib['signed_up'] and lib['index_selected'] > 0:
