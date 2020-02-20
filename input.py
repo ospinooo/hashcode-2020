39b25b07bbd1f820658c18161e3cfdab30c856a4
Ospino <ospino.p@gmail.com>
2020-02-20 19:16:27 +0100

    parse input
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- input.py
+++ input.py
@@ -8,14 +8,24 @@ def parse_input(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
 
-    value = lines[0]
-    value2 = lines[1]
+    # B: Number of different books
+    # L: Number of libraries
+    # D: Number of days
+    B, L, D = [int(value) for value in lines[0].split()]
+    
+    scores = [int(value) for value in lines[1].split()]
 
-    dataset = []
-    for line in lines[2:]:
+    libraries = []
+    for i in range(0, 2*L, 2):
         # Strip -> Get rid of 0s
         # Split -> split with the space into a list
-        line
-        dataset.append()
+        N, T, M = lines[i].split()
 
-    return dataset
+        libraries.append({
+            'total_books':N,
+            'sign_up_days': T, 
+            'books_day':M,
+            'books' : [int(value) for value in lines[i+1].split()]
+        })
+
+    return libraries, scores, D
